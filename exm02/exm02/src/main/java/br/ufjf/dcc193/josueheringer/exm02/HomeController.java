026fcbf7fcc2d4fb2e08e6cba0f7acf3f56b366d
Josué Heringer <josuedelgadoheringer98@gmail.com>
2022-05-03 22:20:18 -0300

    Implementando formulário e exibindo na view

--- exm02/exm02/src/main/java/br/ufjf/dcc193/josueheringer/exm02/HomeController.java
+++ exm02/exm02/src/main/java/br/ufjf/dcc193/josueheringer/exm02/HomeController.java
@@ -2,11 +2,26 @@ package br.ufjf.dcc193.josueheringer.exm02;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
     @RequestMapping({ "", "index.html" })
     public String home() {
         return "home";
+    }
+
+    @RequestMapping({ "formulario", "formulario.html" })
+    public String formulario() {
+        return "form/formulario";
+    }
+
+    @RequestMapping({ "resultado", "resultado.html" })
+    public ModelAndView resultado(String name, Integer age) {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("form/resultado");
+        mv.addObject("age", age);
+        mv.addObject("name", name);
+        return mv;
     }
 }
---
+++ exm02/exm02/src/main/webapp/WEB-INF/views/form/resultado.jsp
@@ -0,0 +1,18 @@
+<%@page pageEncoding="UTF-8" %>
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>EXM: Resultado</title>
+
+
+    <body>
+        <h1>EXM02: Resultado</h1>
+        <p>Olá ${name}! Você tem ${age} anos!</p>
+    </body>
+
+    </html>
