ee320c208392e30c0856145fdec22623fc857a2b
Josué Heringer <josuedelgadoheringer98@gmail.com>
2022-05-24 22:47:11 -0300

    Removendo arquivos enviados

--- exercicio05/atividade05/src/main/java/br/ufjf/dcc193/atividade05/JogadorRepository.java
+++
@@ -1,7 +0,0 @@
-package br.ufjf.dcc193.atividade05;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JogadorRepository extends JpaRepository<Jogador, Long> {
-
-}
--- exm02/exm02/src/main/java/br/ufjf/dcc193/josueheringer/exm02/PessoaRepository.java
+++
@@ -1,7 +0,0 @@
-package br.ufjf.dcc193.josueheringer.exm02;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
-
-}
