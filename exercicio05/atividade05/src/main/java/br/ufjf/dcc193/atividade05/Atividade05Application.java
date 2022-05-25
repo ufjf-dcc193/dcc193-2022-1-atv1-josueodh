34732c20c7f3d7e912eee9b2e240a7e82d01fe5a
Josué Heringer <josuedelgadoheringer98@gmail.com>
2022-05-24 21:44:29 -0300

    Adicionando Banco de Dados, iniciando create no controller e implementando chamada do repositorio no Application

--- exercicio05/atividade05/src/main/java/br/ufjf/dcc193/atividade05/Atividade05Application.java
+++ exercicio05/atividade05/src/main/java/br/ufjf/dcc193/atividade05/Atividade05Application.java
@@ -2,12 +2,17 @@ package br.ufjf.dcc193.atividade05;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Atividade05Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Atividade05Application.class, args);
+
+		ConfigurableApplicationContext ctx = SpringApplication.run(Atividade05Application.class, args);
+		JogadorRepository rep = ctx.getBean(JogadorRepository.class);
+
+		System.out.println(rep.findAll().toString());
 	}
 
 }
--- exercicio05/atividade05/src/main/java/br/ufjf/dcc193/atividade05/HomeController.java
+++ exercicio05/atividade05/src/main/java/br/ufjf/dcc193/atividade05/HomeController.java
@@ -1,13 +1,31 @@
 package br.ufjf.dcc193.atividade05;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    JogadorService ps;
+
     @RequestMapping({ "", "index.html" })
     public String home() {
         return "home";
+    }
+
+    @RequestMapping({ "create", "create.html" })
+    public String create() {
+        return "create";
+    }
+
+    @RequestMapping({ "resultado", "resultado.html" })
+    public RedirectView resultado(Jogador jogador) {
+        Jogador j = new Jogador(jogador.getNome(), jogador.getTime());
+
+        ps.create(j);
+        return new RedirectView("jogadores.html");
     }
 }
